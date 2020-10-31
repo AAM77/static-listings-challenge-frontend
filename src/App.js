6eb3ab4e0410a73b647017f4cbfde96c4e5a17bd
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-10-31 12:02:58 -0400

    Add basic banner with image, background, and styles for the desktop view without breakpoints.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Banner from './components/layout/Banner';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Banner />
       </div>
     );
   }
---
+++ src/components/layout/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import banner from '../../assets/images/bg-header-desktop.svg';
+
+
+const Banner = () => {
+    return (
+        <div id='banner'>
+            <img src={banner}></img>
+        </div>
+    )
+}
+
+export default Banner;
