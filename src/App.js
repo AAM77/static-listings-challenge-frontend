512b0624aae6050e591e36e8a9891ef7a7a95fdd
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-10-31 13:13:26 -0400

    Add a state to store data from data/data.json and pass it to the JobListings component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.scss';
+import data from './data/data.json';
 import Banner from './components/layout/Banner';
+import JobListings from './components/job_listings/desktop/JobListings';
 
 class App extends Component {
+  state = {
+    jobListings: data,
+  }
+
   render() {
+    const { jobListings } = this.state;
+
     return (
       <div className="App">
         <Banner />
+        <JobListings jobListings={jobListings} />
       </div>
     );
   }
