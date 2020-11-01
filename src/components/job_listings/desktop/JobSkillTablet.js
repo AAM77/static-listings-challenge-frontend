93b4b656c71c679d8814baf60a1771a5a5debf18
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 00:25:28 -0400

    Add a new component (JobSkillTablet) to represent all of the filter/skill tablets that a user will be able to select.

---
+++ src/components/job_listings/desktop/JobSkillTablet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const JobSkillTablet = ({ skillName }) => {
+    return (
+        <div className="skill-tablet">
+            <div><p>{skillName}</p></div>
+        </div>
+    )
+}
+
+export default JobSkillTablet;
