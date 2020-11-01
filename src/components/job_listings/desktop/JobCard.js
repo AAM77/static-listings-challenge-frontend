3572f8bf5bf22bb93c43a69566136fb1807174a1
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 00:16:52 -0400

    Add a category section to the job card that holds all of the skills/filters for a job listing.

--- src/components/job_listings/desktop/JobCard.js
+++ src/components/job_listings/desktop/JobCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import JobLogo from './JobLogo';
 import JobInfoSection from './JobInfoSection';
+import JobCategorySection from './JobCategorySection';
 
 
 function JobCard({ jobListing }) {
@@ -8,6 +9,7 @@ function JobCard({ jobListing }) {
         <div key={jobListing.id} className='card shadow'>
             <JobLogo logoPath={jobListing.logo} />
             <JobInfoSection jobListing={jobListing} />
+            <JobCategorySection jobListing={jobListing} />
         </div>
     );
 }
