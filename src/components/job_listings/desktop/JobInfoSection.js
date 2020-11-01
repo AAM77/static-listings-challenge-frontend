8de9026ed94e246769335044ac1eb1e25946998c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 01:26:45 -0500

    Add functions/logic to handle generating a list of tag names (e.g. featured, new) and using that to generate JobListingTag components. Pass the tag text as a prop.

--- src/components/job_listings/desktop/JobInfoSection.js
+++ src/components/job_listings/desktop/JobInfoSection.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import JobListingTag from './JobListingTag';
 
 
 const JobInfoSection = ({ jobListing }) => {
+
+    const getTagNames = () => {
+        const tagNames = [];
+        if (jobListing.new) {
+            tagNames.push('NEW!');
+        }
+
+        if (jobListing.featured) {
+            tagNames.push('FEATURED');
+        }
+
+        return tagNames;
+    }
+
+    const createJobListingTags = () => {
+        return getTagNames().map( (tagName, index) => <JobListingTag key={index} tagName={tagName} />);
+    }
+
     return (
         <div className='job-info'>
-            <div className='company'>{jobListing.company}</div>
+            <div className='company-and-tags'>
+                <div className='company'>
+                    {jobListing.company}
+                </div>
+                <div className='tags'>
+                    {createJobListingTags()}
+                </div>
+            </div>
             <div className='position'>{jobListing.position}</div>
             <div className='additional-details'>
                 {jobListing.postedAt}<span className='interpunct'>·</span>{jobListing.contract}<span className='interpunct'>·</span>{jobListing.location}
