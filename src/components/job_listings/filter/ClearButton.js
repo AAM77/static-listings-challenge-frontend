c0c10e1e181752b7b9b0c3adeae40590cf6a5daa
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 09:37:47 -0500

    Rename Filter.js to SelectedFilters.js since it will hold the different filter tablets and move it into its own folder. Create a ClearButton.js component that will clear all of the filters when clicked.

---
+++ src/components/job_listings/filter/ClearButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ClearButton = () => {
+    return (
+        <a class="clear-button">
+            <span>Clear</span>
+        </a>
+    );
+}
+
+export default ClearButton;
