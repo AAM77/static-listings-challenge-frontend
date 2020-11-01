d32685e94d778c228264a21d649241eabcc2fa5d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 09:39:05 -0500

    Add the SelectedFilters component to App.js to make it easier to visualize and style.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.scss';
 import data from './data/data.json';
 import Banner from './components/layout/Banner';
 import JobListings from './components/job_listings/desktop/JobListings';
+import SelectedFilters from './components/job_listings/filter/SelectedFilters';
+
 
 class App extends Component {
   state = {
     jobListings: data,
+  }
+
+  filterSelectionHandler = (e) => {
+
   }
 
   render() {
@@ -15,6 +21,7 @@ class App extends Component {
     return (
       <div className="App">
         <Banner />
+        <SelectedFilters />
         <JobListings jobListings={jobListings} />
 
         <div class="attribution footer">
