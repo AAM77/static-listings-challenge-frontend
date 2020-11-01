3324fe15f0e389a5afa3531246192a683f584121
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 01:35:47 -0500

    Remove unnecessary code from index.html, move footer into App.js, and add minor styling to make it look a little nicer.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ class App extends Component {
       <div className="App">
         <Banner />
         <JobListings jobListings={jobListings} />
+
+        <div class="attribution footer">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="#">Mohammad Adeel</a>.
+        </div>
       </div>
     );
   }
