b927807dfc1485fe4fe3d639530237fcbd226bc2
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 00:24:25 -0400

    Nested logo image in another container to provide a little extra control.

--- src/components/job_listings/desktop/JobLogo.js
+++ src/components/job_listings/desktop/JobLogo.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 const JobLogo = ({ logoPath }) => {
     return (
-        <div id='logo'>
-            <img src={process.env.PUBLIC_URL + logoPath} alt="job logo"></img>
+        <div className='logo'>
+            <div id='image-container'>
+                <img src={process.env.PUBLIC_URL + logoPath} alt="job logo"></img>
+            </div>
         </div>
     )
 }
