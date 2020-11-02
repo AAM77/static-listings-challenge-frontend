0dc3dc1a1c702b3a02fe8b114c3d321d93c0f834
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-01 20:48:43 -0500

    Pass selectedFilters to the job listings component and create a function that returns job listings that match all of the criteria, if present.

--- src/components/job_listings/desktop/JobListings.js
+++ src/components/job_listings/desktop/JobListings.js
@@ -2,10 +2,36 @@ import React from 'react';
 import JobCard from './JobCard';
 
 
-const JobListings = ({ jobListings, selectFilter }) => {
+const JobListings = ({ jobListings, selectFilter, selectedFilters }) => {
+
+    const getFilteredJobListings = () => {
+        /*
+        Mohammad Adeel - November, 1, 2020.
+        Retrieves only those job listings that match the skillNames of all of the selected filters.
+        If there are no selected filters, it returns all of the jobListings.
+        */
+        if (selectedFilters.length > 0) {
+            return jobListings.filter(jobListing => {
+                for (const selectedFilter of selectedFilters) {
+                    if (['tools','languages'].includes(selectedFilter.category)) {
+                        if (!jobListing[selectedFilter.category].includes(selectedFilter.skillName)) {
+                            return false;
+                        }
+                    } else if (['role', 'level'].includes(selectedFilter.category)) {
+                        if (jobListing[selectedFilter.category] !== selectedFilter.skillName) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            })
+        }
+        return jobListings;
+    }
+
     return (
         <div className='container' id='job-listings'>
-            { jobListings.map( jobListing => <JobCard jobListing={jobListing} selectFilter={selectFilter}  />) }
+            { getFilteredJobListings().map( jobListing => <JobCard jobListing={jobListing} selectFilter={selectFilter}  />) }
         </div>
     );
 }
