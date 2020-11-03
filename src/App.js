342e0d46ba23f9f530875895f0edbf79525fb84c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-03 18:35:18 -0500

    Add comments and clean up code for App.js.

--- src/App.js
+++ src/App.js
@@ -11,16 +11,15 @@ class App extends Component {
     jobListings: data,
     selectedFilters: [], // Should be an array of objects that contain both the category and the skill name.
     isSelectedFilters: false,
-    browserWidth: Math.max(
-      document.body.scrollWidth,
-      document.documentElement.scrollWidth,
-      document.body.offsetWidth,
-      document.documentElement.offsetWidth,
-      document.documentElement.clientWidth
-    ),
+    browserWidth: null,
   }
 
   getBrowserWidth = () => {
+    /* Mohammad Adeel - November 3, 2020.
+    Returns a close estimate of the device's 
+    width. This is currently being used to 
+    replace banner image depending on the
+    browser width, */
     return Math.max(
       document.body.scrollWidth,
       document.documentElement.scrollWidth,
@@ -31,52 +30,84 @@ class App extends Component {
   }
 
   setBrowserWidth = () => {
+    /* Mohammad Adeel - November 3, 2020. Sets 
+    the current browser wide to the state */
     const browserWidth = this.getBrowserWidth();
     this.setState({browserWidth: browserWidth});
   }
 
   setSelectedFiltersStatus = () => {
+    /* Mohammad Adeel - November 3, 2020. Determines
+    if the user has selected filters by the length
+    of the array of selected filters as a boolean.
+    Used as a trigger to display/hide the filter 
+    component. */
     const hasSelectedFilters = this.state.selectedFilters.length <= 0 ? false : true;
     this.setState({isSelectedFilters: hasSelectedFilters});
   }
 
+  isFilters = (selectedFilters) => {
+    /* Mohammad Adeel - November 3, 2020. Determines if
+    the user has selected any filters. */
+    return selectedFilters.length > 0 ? true : false
+  }
+
   addFilterToState = (event) => {
+    /* Mohammad Adeel - November 3, 2020. Retrieves the category
+    and skillname from the component and uses it to create a filter.
+    Used to add the filter to the state's selectedFilters array.
+    This is used to create and add the appropriate Filter-tag component
+    to the SelectedFilters component. */
     const category = event.target.getAttribute('data-category');
     const skillName = event.target.getAttribute('data-skillname');
 
+    /* Mohammad Adeel - November 3, 2020. Prevent duplicates from
+    getting added to the state. */
     const isNotSelected = this.state.selectedFilters.every( selectedFilter => selectedFilter.skillName !== skillName);
 
     if (isNotSelected) {
       const newSelectedFilters = [...this.state.selectedFilters, {category: category, skillName: skillName}];
-      const isFilters = newSelectedFilters.length > 0 ? true : false;
-      this.setState({selectedFilters: newSelectedFilters, isSelectedFilters: isFilters});
+
+      this.setState({
+        selectedFilters: newSelectedFilters, 
+        isSelectedFilters: this.isFilters(newSelectedFilters)
+      });
     }
-    
   }
 
   removeFilterFromState = (event) => {
+    /* Mohammad Adeel - November 3, 2020. Remove the selected 
+    filter-tag from the state based on the category and skillname 
+    from the retrieved element. */
+
     const selectedFilters = [...this.state.selectedFilters];
+
+    /* Mohammad Adeel - November 3, 2020. Retrieves the index of the JS 
+    object representing the selected filter. */
     const targetFilterIndex = selectedFilters.findIndex( (selectedFilter) => (
       selectedFilter.category === event.target.getAttribute('data-category') &&
       selectedFilter.skillName === event.target.getAttribute('data-skillname')
     ));
 
+    /* Mohammad Adeel - November 3, 2020. Splices out/removes the target
+    object/filter from the copied array. */
     if (targetFilterIndex !== -1) {
       selectedFilters.splice(targetFilterIndex, 1)
     }
-     
-     const isFilters = selectedFilters.length > 0 ? true : false;
-     this.setState({selectedFilters: selectedFilters, isSelectedFilters: isFilters});
+     this.setState({selectedFilters: selectedFilters, isSelectedFilters: this.isFilters(selectedFilters)});
   }
 
   clearSelectedFilters = () => {
-    const newSelectedFilters = [];
-    const isFilters = newSelectedFilters.length > 0 ? true : false;
-    this.setState({selectedFilters: newSelectedFilters, isSelectedFilters: isFilters});
+    /* Mohammad Adeel - November 3, 2020. Used to clear all filters upon clicking. */
+    const selectedFilters = [];
+    this.setState({selectedFilters: selectedFilters, isSelectedFilters: this.isFilters(selectedFilters)});
   }
 
   componentDidMount = () => {
+    /* Mohammad Adeel - November 3, 2020. Sets initial values and
+    adds eventhandlers to the DOM. */
     this.setSelectedFiltersStatus();
+    this.setBrowserWidth();
     window.addEventListener('resize', this.setBrowserWidth);
   }
 
