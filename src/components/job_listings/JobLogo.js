ceff1af45e2250a8a8fdd01dda0cb81a7115b0e4
Mohammad Adeel <adeel.a.mhd@gmail.com>
2020-11-02 20:50:51 -0500

    Remove extra div/container from the logo component.

--- src/components/job_listings/JobLogo.js
+++ src/components/job_listings/JobLogo.js
@@ -4,9 +4,7 @@ import React from 'react';
 const JobLogo = ({ logoPath }) => {
     return (
         <div className='logo'>
-            <div id='image-container'>
-                <img src={process.env.PUBLIC_URL + logoPath} alt="job logo"></img>
-            </div>
+            <img src={process.env.PUBLIC_URL + logoPath} alt="job logo"></img>
         </div>
     )
 }
